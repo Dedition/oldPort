22837b2814c67a8e6be9487b2c39e1f595a01cce
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-08-18 12:09:05 -0400

    Fixed homepage

--- leo-portfolio/src/components/MenuPage/MenuList.js
+++ leo-portfolio/src/components/MenuPage/MenuList.js
@@ -11,140 +11,138 @@ const MenuList = () => {
     const [isSkillHovered, setIsSkillHovered] = useState(false);
 
     return (
-        <div className="MenuList">
-            <ul className="MenuList__list">
-                <li
-                    onMouseEnter={() => {
-                        setMenuText("About");
-                        setIsAboutHovered(true);
-                        setIsProjectHovered(false);
-                        setIsWorkHovered(false);
-                        setIsContactHovered(false);
-                        setIsSkillHovered(false)
-                    }}
-                    onMouseLeave={() => setMenuText("")}
-                    className="MenuList__item"
-                >
-                    <Link to="/about" className="MenuList__link">
-                        {isAboutHovered ? (
-                            <p className="primaryText menuTextListAnimation">
-                                About
-                            </p>
-                        ) : (
-                            <p className="secondaryText menuTextListAnimation">
-                                About
-                            </p>
-                        )
-                        }
-                    </Link>
-                    <div className={isAboutHovered ? "MenuList__indicator" : "MenuList__indicator--hidden"}></div>
-                </li>
-                <li
-                    onMouseEnter={() => {
-                        setMenuText("Work")
-                        setIsWorkHovered(true);
-                        setIsProjectHovered(false);
-                        setIsContactHovered(false);
-                        setIsAboutHovered(false);
-                        setIsSkillHovered(false)
+        <ul className="MenuList__list">
+            <li
+                onMouseEnter={() => {
+                    setMenuText("About");
+                    setIsAboutHovered(true);
+                    setIsProjectHovered(false);
+                    setIsWorkHovered(false);
+                    setIsContactHovered(false);
+                    setIsSkillHovered(false)
+                }}
+                onMouseLeave={() => setMenuText("")}
+                className="MenuList__item"
+            >
+                <Link to="/about" className="MenuList__link">
+                    {isAboutHovered ? (
+                        <p className="primaryText ">
+                            About
+                        </p>
+                    ) : (
+                        <p className="secondaryText ">
+                            About
+                        </p>
+                    )
                     }
-                    }
-                    onMouseLeave={() => setMenuText("")}
-                    className="MenuList__item"
-                >
-                    <Link to="/work" className="MenuList__link">
-                        {isWorkHovered ? (
-                            <p className="primaryText menuTextListAnimation">
-                                Work
-                            </p>
-                        ) : (
-                            <p className="secondaryText menuTextListAnimation">
-                                Work
-                            </p>
-                        )}
-                    </Link>
-                    <div className={isWorkHovered ? "MenuList__indicator" : "MenuList__indicator--hidden"}></div>
-                </li>
-                <li
-                    onMouseEnter={() => {
-                        setMenuText("Skills");
-                        setIsSkillHovered(true)
-                        setIsProjectHovered(false);
-                        setIsWorkHovered(false);
-                        setIsContactHovered(false);
-                        setIsAboutHovered(false);
-                    }}
-                    onMouseLeave={() => setMenuText("")}
-                    className="MenuList__item"
-                >
-                    <Link to="/skills" className="MenuList__link">
-                        {isSkillHovered ? (
-                            <p className="primaryText menuTextListAnimation">
-                                Skills
-                            </p>
-                        ) : (
-                            <p className="secondaryText menuTextListAnimation">
-                                Skills
-                            </p>
-                        )}
-                    </Link>
-                    <div className={isSkillHovered ? "MenuList__indicator" : "MenuList__indicator--hidden"}></div>
-                </li>
+                </Link>
+                <div className={isAboutHovered ? "MenuList__indicator" : "MenuList__indicator--hidden"}></div>
+            </li>
+            <li
+                onMouseEnter={() => {
+                    setMenuText("Work")
+                    setIsWorkHovered(true);
+                    setIsProjectHovered(false);
+                    setIsContactHovered(false);
+                    setIsAboutHovered(false);
+                    setIsSkillHovered(false)
+                }
+                }
+                onMouseLeave={() => setMenuText("")}
+                className="MenuList__item"
+            >
+                <Link to="/work" className="MenuList__link">
+                    {isWorkHovered ? (
+                        <p className="primaryText ">
+                            Work
+                        </p>
+                    ) : (
+                        <p className="secondaryText ">
+                            Work
+                        </p>
+                    )}
+                </Link>
+                <div className={isWorkHovered ? "MenuList__indicator" : "MenuList__indicator--hidden"}></div>
+            </li>
+            <li
+                onMouseEnter={() => {
+                    setMenuText("Skills");
+                    setIsSkillHovered(true)
+                    setIsProjectHovered(false);
+                    setIsWorkHovered(false);
+                    setIsContactHovered(false);
+                    setIsAboutHovered(false);
+                }}
+                onMouseLeave={() => setMenuText("")}
+                className="MenuList__item"
+            >
+                <Link to="/skills" className="MenuList__link">
+                    {isSkillHovered ? (
+                        <p className="primaryText">
+                            Skills
+                        </p>
+                    ) : (
+                        <p className="secondaryText">
+                            Skills
+                        </p>
+                    )}
+                </Link>
+                <div className={isSkillHovered ? "MenuList__indicator" : "MenuList__indicator--hidden"}></div>
+            </li>
+            <div className={isContactHovered ? "MenuList__indicator" : "MenuList__indicator--hidden"}></div>
+            <li
+                onMouseEnter={() => {
+                    setMenuText("Projects");
+                    setIsProjectHovered(true);
+                    setIsWorkHovered(false);
+                    setIsContactHovered(false);
+                    setIsAboutHovered(false);
+                    setIsSkillHovered(false)
+                }
+                }
+                onMouseLeave={() => setMenuText("")}
+                className="MenuList__item"
+            >
+                <Link to="/projects" className="MenuList__link">
+                    {isProjectHovered ? (
+                        <p className="primaryText">
+                            Projects
+                        </p>
+                    ) : (
+                        <p className="secondaryText">
+                            Projects
+                        </p>
+                    )}
+                </Link>
+                <div className={isProjectHovered ? "MenuList__indicator" : "MenuList__indicator--hidden"}></div>
+            </li>
+            <li
+                onMouseEnter={() => {
+                    setMenuText("Contact");
+                    setIsContactHovered(true);
+                    setIsProjectHovered(false);
+                    setIsWorkHovered(false);
+                    setIsAboutHovered(false);
+                    setIsSkillHovered(false)
+                }
+                }
+                onMouseLeave={() => setMenuText("")}
+                className="MenuList__item"
+            >
+                <Link to="/contact" className="MenuList__link">
+                    {isContactHovered ? (
+                        <p className="primaryText">
+                            Contact
+                        </p>
+                    ) : (
+                        <p className="secondaryText">
+                            Contact
+                        </p>
+                    )}
+                </Link>
                 <div className={isContactHovered ? "MenuList__indicator" : "MenuList__indicator--hidden"}></div>
-                <li
-                    onMouseEnter={() => {
-                        setMenuText("Projects");
-                        setIsProjectHovered(true);
-                        setIsWorkHovered(false);
-                        setIsContactHovered(false);
-                        setIsAboutHovered(false);
-                        setIsSkillHovered(false)
-                    }
-                    }
-                    onMouseLeave={() => setMenuText("")}
-                    className="MenuList__item"
-                >
-                    <Link to="/projects" className="MenuList__link">
-                        {isProjectHovered ? (
-                            <p className="primaryText menuTextListAnimation">
-                                Projects
-                            </p>
-                        ) : (
-                            <p className="secondaryText menuTextListAnimation">
-                                Projects
-                            </p>
-                        )}
-                    </Link>
-                    <div className={isProjectHovered ? "MenuList__indicator" : "MenuList__indicator--hidden"}></div>
-                </li>
-                <li
-                    onMouseEnter={() => {
-                        setMenuText("Contact");
-                        setIsContactHovered(true);
-                        setIsProjectHovered(false);
-                        setIsWorkHovered(false);
-                        setIsAboutHovered(false);
-                        setIsSkillHovered(false)
-                    }
-                    }
-                    onMouseLeave={() => setMenuText("")}
-                    className="MenuList__item"
-                >
-                    <Link to="/contact" className="MenuList__link">
-                        {isContactHovered ? (
-                            <p className="primaryText menuTextListAnimation">
-                                Contact
-                            </p>
-                        ) : (
-                            <p className="secondaryText menuTextListAnimation">
-                                Contact
-                            </p>
-                        )}
-                    </Link>
-                    <div className={isContactHovered ? "MenuList__indicator" : "MenuList__indicator--hidden"}></div>
-                </li>
-            </ul>
-        </div >
+            </li>
+        </ul>
     );
 }
 
--- leo-portfolio/src/components/MenuPage/MenuPage.js
+++ leo-portfolio/src/components/MenuPage/MenuPage.js
@@ -8,13 +8,14 @@ const MenuPage = () => {
             <div className="MenuPage__container"
                 onMouseEnter={() => setMenuText("Home")}
             >
+
                 {menuText === "Home" || menuText === "" ? (
                     <div className="MenuPage__text">
                         <p className="primaryText menuTextListAnimation">
                             Hello, I'm Leo, your friendly neighborhood web developer. But you can just call
                             me your next hire. It's my ever-growing pleasure to make your acquaintance!
                         </p>
-                        <p className="secondaryText menuTextListAnimation">
+                        <p className="secondaryText">
                             I'm a full-stack web developer with a passion for creating
                             beautiful, responsive websites.
                         </p>
@@ -25,7 +26,7 @@ const MenuPage = () => {
                             Here's a selection of my projects I did during my free time and during some collaborations
                             with other developers.
                         </p>
-                        <p className="secondaryText menuTextListAnimation">
+                        <p className="secondaryText">
                             Projects
                         </p>
                     </div>
@@ -34,7 +35,7 @@ const MenuPage = () => {
                         <p className="primaryText menuTextListAnimation">
                             Here's a selection of my skills and technologies I use in my role as a Software Engineer.
                         </p>
-                        <p className="secondaryText menuTextListAnimation">
+                        <p className="secondaryText">
                             Skills
                         </p>
                     </div>
@@ -44,7 +45,7 @@ const MenuPage = () => {
                             This is only if you're more interested in things other than my life as a web developer.
                             Get to know me a little better.
                         </p>
-                        <p className="secondaryText menuTextListAnimation">
+                        <p className="secondaryText">
                             About Me
                         </p>
                     </div>
@@ -53,7 +54,7 @@ const MenuPage = () => {
                         <p className="primaryText menuTextListAnimation">
                             Feel free to reach out to me if you have any questions or want to work together.
                         </p>
-                        <p className="secondaryText menuTextListAnimation">
+                        <p className="secondaryText">
                             Contact
                         </p>
                     </div>
@@ -62,7 +63,7 @@ const MenuPage = () => {
                         <p className="primaryText menuTextListAnimation">
                             If you were looking for my work experience, here you go!
                         </p>
-                        <p className="secondaryText menuTextListAnimation">
+                        <p className="secondaryText">
                         </p>
                     </div>
                 ) : null}
