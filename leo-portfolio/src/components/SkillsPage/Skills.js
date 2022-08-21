cc2476ac162d467c9a80806f14203302c7e299bd
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-08-21 17:09:17 -0400

    Added skills

--- leo-portfolio/src/components/SkillsPage/Skills.js
+++ leo-portfolio/src/components/SkillsPage/Skills.js
@@ -16,15 +16,15 @@ const Skills = () => {
         <motion.main className='mainContainer mainSkills' style={anim} exit={{ opacity: 0 }}>
             <h2>Skills & Technologies</h2>
             <div className='skillsContainer'>
-                <span className="technologies__html">HTML5</span>
-                <span className="technologies__css">CSS3</span>
-                <span className="technologies__js">Javascript</span>
-                <span className="technologies__sass">Sass</span>
-                <span className="technologies__react">React</span>
-                <span className="technologies__node">NodeJS</span>
-                <span className="technologies__heroku">Heroku</span>
-                <span className="technologies__git">Git</span>
-                <span className="technologies__github">Github</span>
+                <span className="skillsContainer__html">HTML5</span>
+                <span className="skillsContainer__css">CSS3</span>
+                <span className="skillsContainer__js">Javascript</span>
+                <span className="skillsContainer__sass">Sass</span>
+                <span className="skillsContainer__react">React</span>
+                <span className="skillsContainer__node">NodeJS</span>
+                <span className="skillsContainer__heroku">Heroku</span>
+                <span className="skillsContainer__git">Git</span>
+                <span className="skillsContainer__github">Github</span>
             </div>
         </motion.main>
     );
