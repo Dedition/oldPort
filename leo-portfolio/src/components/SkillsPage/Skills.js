8858ade262c672341d69f799695a7be91e8d7a04
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-08-21 20:22:35 -0400

    Added skills section

--- leo-portfolio/src/components/SkillsPage/Skills.js
+++ leo-portfolio/src/components/SkillsPage/Skills.js
@@ -16,15 +16,34 @@ const Skills = () => {
         <motion.main className='mainContainer mainSkills' style={anim} exit={{ opacity: 0 }}>
             <h2>Skills & Technologies</h2>
             <div className='skillsContainer'>
-                <span className="skillsContainer__html">HTML5</span>
-                <span className="skillsContainer__css">CSS3</span>
+                <span className="skillsContainer__python">Python</span>
                 <span className="skillsContainer__js">Javascript</span>
-                <span className="skillsContainer__sass">Sass</span>
-                <span className="skillsContainer__react">React</span>
+                <span className="skillsContainer__npm">NPM</span>
                 <span className="skillsContainer__node">NodeJS</span>
+                <span className="skillsContainer__html">HTML5</span>
+                <span className="skillsContainer__sass">Sass</span>
+                <span className="skillsContainer__css">CSS3</span>
+                <span className="skillsContainer__pug">PugJS</span>
+                <span className="skillsContainer__react">ReactJS</span>
+                <span className="skillsContainer__redux">Redux</span>
+                <span className="skillsContainer__react">React-Router</span>
+                <span className="skillsContainer__react">React-Spring</span>
+                <span className="skillsContainer__react">React-Hooks</span>
+                <span className="skillsContainer__flask">Flask</span>
+                <span className="skillsContainer__flask">Flask-SQLAlchemy</span>
+                <span className="skillsContainer__postgres">PostgreSQL</span>
+                <span className="skillsContainer__js">Express.js</span>
+                <span className="skillsContainer__sequelize">Sequelize</span>
+                {/* <span className="skillsContainer__js">NodeJS</span> */}
+                <span className="skillsContainer__docker">Docker</span>
+                <span className="skillsContainer__aws">AWS S3</span>
                 <span className="skillsContainer__heroku">Heroku</span>
-                <span className="skillsContainer__git">Git</span>
                 <span className="skillsContainer__github">Github</span>
+                <span className="skillsContainer__git">Git</span>
+                <span className="skillsContainer__scrum">Test-Driven Development</span>
+                <span className="skillsContainer__scrum">SCRUM</span>
+                <span className="skillsContainer__scrum">Kanban</span>
+                <span className="skillsContainer__api">REST API</span>
             </div>
         </motion.main>
     );
